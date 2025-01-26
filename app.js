d9aa67730a797ffc4887fa885c2823a01f785266
Saad Ahmed Khan <saadahmedkhan578@gmail.com>
2025-01-27 00:42:21 +0500

    Task Add

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+async function checkSession(){
+    try {
+        const {data , error} = await supabase.auth.getSession();
+        const authPages = ["index.html" , "login.html" , ""]
+        const currentPath = window.location.pathname
+        const isauthPage = authPages.some((page) => page.includes(currentPath))
+        const session = {data}
+        if(session){
+            if(isauthPage){
+                window.location.href = "dashbord.html"
+            }
+        }
+        else{
+            if(!isauthPage){
+                window.location.href = "login.html"
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
 
+window.onload = checkSession;
